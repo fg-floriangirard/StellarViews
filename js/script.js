4f63900b3e73d14c9abb1e8433699185cb53564b
fg-floriangirard <fg.girardflorian@gmail.com>
2023-07-19 18:47:54 +0200

    Add slider functionality for photos

--- js/script.js
+++ js/script.js
@@ -7,11 +7,39 @@
  */
 function changeTheme() {
   const body = document.querySelector('body');
-  
+
   if (body.classList.contains('theme-dark')) {
     body.classList.remove('theme-dark');
   }
   else {
     body.classList.add('theme-dark');
   }
+}
+
+/**
+ * Generates slider images and initializes the slider.
+ * This function creates image elements for each image filename in the sliderImages array,
+ * sets their source and alt attributes, and adds them to the slider container.
+ * The first image is set as the current image (visible in the slider).
+ */
+function generateSliderImages() {
+  const sliderImages = [
+      'saturn.jpg',
+      'jupiter.jpg',
+      'carina.jpg'
+   ];
+
+   const sliderContainer = document.querySelector('.slider');
+   // Loop through each image filename in the sliderImages array
+   for (const current of sliderImages) {
+      const newSliderImage = document.createElement('img');     
+      newSliderImage.src = "img/"+current;     
+      newSliderImage.classList.add("slider__img");
+      newSliderImage.alt = "Partir à la plage";
+      // Prepend the newly created image to the slider container
+      sliderContainer.prepend(newSliderImage);
+   }
+   // Set the first image as the current image (visible in the slider)
+   const firstSliderImage = document.querySelector('.slider__img');
+   firstSliderImage.classList.add('slider__img--current');
 }
